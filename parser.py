259eea45aa1dda9e2560284e88ae4226aa800082
Abineshkumar K <akumarav@stevens.edu>
2017-03-02 15:18:29 -0500

    Week 7 - NGrams
    Adjective, Noun etc

--- parser.py
+++ parser.py
@@ -1,93 +1,108 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Feb 13 16:07:40 2017
+Created on Mon Feb 27 16:01:05 2017
 
-@author: abineshkumar Kumaravel
+@author: abine
 """
-
-from bs4 import BeautifulSoup
+import nltk
+from nltk.util import ngrams
 import re
-import time
-import requests
+from nltk.tokenize import sent_tokenize
+from nltk import load
 
-def getCritic(review):
-    critic='NA' # initialize critic and text 
-    criticChunk=review.find('a',{'href':re.compile('/critic/')})
-    if criticChunk: critic=criticChunk.text#.encode('ascii','ignore')
-    return critic
+#getTop3 function
+list1=[] #empty list definiton
+def getTop3(D):
+    for key in sorted(D, key=D.get, reverse=True)[:3]:
+        list1.append(key) #appending the key to list
+    return list1 #returning list
+d={"table":20,"chair":12,"pen":10,"pencil":23,"notebook":21,"Book":41} #dictionary definition
+print(getTop3(d))  #function call
 
-def getRating(review):
-    rating = 'NA' #Initailize rating to NA
-    if (review.find('div',{'class':'review_icon icon small fresh'})):
-        return ('fresh')
-    elif (review.find('div',{'class':'review_icon icon small rotten'})):
-        return ('rotten')
-    else:
-        return rating
+def loadLexicon(fname):
+    newLex=set()
+    lex_conn=open(fname)
+    #add every word in the file to the set
+    for line in lex_conn:
+        newLex.add(line.strip())# remember to strip to remove the lin-change character
+    lex_conn.close()
 
-
-def getSource(review):
-    source = 'NA' #Initailize rating to NA
-    sourceChunk = review.find('em',{'class':'subtle'})
-    if sourceChunk:
-        source = sourceChunk.text
-    return source
+    return newLex
     
-def getDate(review):
-    date = 'NA' #Initailize rating to NA
-    dateChunk = review.find('div',{'class':'review_date'})
-    if dateChunk:
-        date = dateChunk.text
-    return date
+def processSentence(sentence,posLex,negLex,tagger):
+    result=[]
+    terms = nltk.word_tokenize(sentence.lower())
+    POStags=['NN'] # POS tags of interest 		
+    POSterms=getPOSterms(terms,POStags,tagger)
+    noun=POSterms['NN']
+    posneg = posLex|negLex    
+    fourgrams = ngrams(terms, 4)
+    for tg in fourgrams:  
+        if tg[0]=='not' and tg[2] in posneg and tg[3] in noun: # if the 2gram is a an adverb followed by an adjective
+             result.append(tg)
+    return result
 
-    
 
-def getTextLen(review):
-    text='NA'
-    textChunk=review.find('div',{'class':'the_review'})
-    if textChunk: 
-        text=textChunk.text#.encode('ascii','ignore')
-        text=text.strip()
-        return len(text)
-    else:   
-        return text
+# return all the terms that belong to a specific POS type
+def getPOSterms(terms,POStags,tagger):
+	
+    tagged_terms=tagger.tag(terms)#do POS tagging on the tokenized sentence
 
-def run(url):
+    POSterms={}
+    for tag in POStags:POSterms[tag]=set()
 
-    pageNum=1 # number of pages to collect
+    #for each tagged term
+    for pair in tagged_terms:
+        for tag in POStags: # for each POS tag 
+            if pair[1].startswith(tag): POSterms[tag].add(pair[0])
 
- 	
-    for p in range(1,pageNum+1): # for each page 
+    return POSterms
 
-        print ('page',p)
-        html=None
 
-        if p==1: pageLink=url # url for page 1
-        else: pageLink=url+'?page='+str(p)+'&sort=' # make the page url
-		
-        for i in range(5): # try 5 times
-            try:
-                #use the browser to access the url
-                response=requests.get(pageLink,headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36', })
-                html=response.content # get the html
-                break # we got the file, break the loop
-            except Exception as e:# browser.open() threw an exception, the attempt to get the response failed
-                print ('failed attempt',i)
-                time.sleep(2) # wait 2 secs
-				
-		
-        if not html:continue # couldnt get the page, ignore
+def run(fpath):
+
+    #make a new tagger
+    _POS_TAGGER = 'taggers/maxent_treebank_pos_tagger/english.pickle'
+    tagger = load(_POS_TAGGER)
+
+    #read the input
+    f=open(fpath)
+    text=f.read().strip()
+    f.close()
+
+    #split sentences
+    sentences=sent_tokenize(text)
+    #print ('NUMBER OF SENTENCES: ',len(sentences))
+
+    result=[]
+
+    posLex=loadLexicon('positive-words.txt')
+    negLex=loadLexicon('negative-words.txt')
+
+    # for each sentence
+    for sentence in sentences:
+
+        sentence=re.sub('[^a-zA-Z\d]',' ',sentence)#replace chars that are not letters or numbers with a spac
+        sentence=re.sub(' +',' ',sentence).strip()#remove duplicate spaces
+
+        #tokenize the sentence
+           
+
         
-        soup = BeautifulSoup(html.decode('ascii', 'ignore'),'lxml') # parse the html 
+        #posneg=posLex | negLex        
+        #POStags=['NN'] # POS tags of interest 		
+        #POSterms=getPOSterms(terms,POStags,tagger)
 
-        reviews=soup.findAll('div', {'class':re.compile('review_table_row')}) # get all the review divs
+        #noun=POSterms['NN']
+        
+        result+=processSentence(sentence,posLex,negLex,tagger)
+        #get the results for this sentence 
+        #adjAfterAdv+=getAdvAdjTwograms(terms, adjectives, adverbs)
+        
+    return result
 
-        for review in reviews:
 
-            
-            print(getCritic(review),' ',getRating(review),' ',getSource(review),' ',getDate(review),' ',getTextLen(review))
- 
- 
 if __name__=='__main__':
-    url='https://www.rottentomatoes.com/m/space_jam/reviews/'
-    run(url)
+	print (run('input.txt'))
+
+    
